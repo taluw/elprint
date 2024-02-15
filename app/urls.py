e4943ecf873cecdfdfa079175ee4073ae024da04
taluw <electsteve6@gmail.com>
2024-02-15 09:24:38 +0500

    Хорошо, 9 утра. Работает, но я - нет

--- app/urls.py
+++ app/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path( 'contact/', views.contact, name='contact'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,15 +4,26 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Main page E&L Print',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'is_authenticated': False
+        'title': 'Home - Главная',
+        'content': 'Главная страница E&L Print'
     }
 
     return render(request, 'main/index.html', context)
 
-
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': "Красивый текст про Лену и Леонида"
+    }
+
+    return render(request, 'main/about.html', context)    
+
+def contact(request):
+    context = {
+        'title': 'Home - Контакты',
+        'content': 'Контакты',
+        'text_on_page': "Ну контакты тудыть сюдыть"
+    }
+
+    return render(request, 'main/contact.html', context)    

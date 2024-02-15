e62d30a385c270536e4b09f8e43b4c1496f4c060
taluw <electsteve6@gmail.com>
2024-02-15 21:07:37 +0500

    Меня выебала настройка таблиц, а могла бы ты...

--- app/settings.py
+++ app/settings.py
@@ -106,8 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
+#en-us default
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,12 @@
+from tabnanny import verbose
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'

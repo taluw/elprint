ff746a003e725161636558d9db32a8dcaf4acf70
taluw <electsteve6@gmail.com>
2024-02-23 19:16:09 +0500

    +full text search with ranking

--- goods/utils.py
+++ goods/utils.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from goods.models import Products
 
@@ -6,13 +7,19 @@ from goods.models import Products
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
     
-    keywords = [word for word in query.split() if len(word) > 2]
 
-    q_objects = Q()
+#def q_search(query):
+#    if query.isdigit() and len(query) <= 5:
+#        return Products.objects.filter(id=int(query))
+#    return Products.objects.annotate(rank=SearchVector("name", "description")).filter(search=query)
 
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
-
-    return Products.objects.filter(q_objects)
+    #keywords = [word for word in query.split() if len(word) > 2]
+    #q_objects = Q()
+    #for token in keywords:
+    #    q_objects |= Q(description__icontains=token)
+    #    q_objects |= Q(name__icontains=token)
+    #return Products.objects.filter(q_objects)

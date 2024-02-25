8409336c15218b91de456260f20c807b7d0a87dc
taluw <electsteve6@gmail.com>
2024-02-25 23:35:35 +0500

    Что делать, Фауст? Таков вам положен предел, Его ж никто не преступает. // profile form controller+notifications

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
-from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from traitlets import Instance
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -15,6 +17,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f"Успешный вход в аккаунт {username}")
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -34,6 +37,7 @@ def registration(request):
             form.save()
             user = form.instance
             auth.login(request, user)
+            messages.success(request, f"Успешная регистрация {user.username}")
             return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserRegistrationForm()
@@ -44,14 +48,27 @@ def registration(request):
 
     return render(request, 'users/registration.html', context)
 
+@login_required
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Профиль успешно обновлен")
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+
     context = {
         'title': 'Home - Кабинет',
+        'form': form,
         
     }
 
     return render(request, 'users/profile.html', context)
 
+@login_required
 def logout(request):
+    messages.success(request, "Вы вышли из аккаунта")
     auth.logout(request)
     return redirect(reverse('main:index'))

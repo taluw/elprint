c5f45f49332fd14d94cf5c182c2e5836f92c686e
taluw <electsteve6@gmail.com>
2024-02-25 21:03:27 +0500

    Одиозность нынешнего поколения заставляет задуматься: а действительно ли воспитание во время советсткого союза было излишне строгим? // reg ready

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -27,8 +27,19 @@ def login(request):
     return render(request, 'users/login.html', context)
 
 def registration(request):
+
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
     context = {
         'title': 'Home - Регистрация',
+        'form': form
     }
 
     return render(request, 'users/registration.html', context)
@@ -36,9 +47,11 @@ def registration(request):
 def profile(request):
     context = {
         'title': 'Home - Кабинет',
+        
     }
 
     return render(request, 'users/profile.html', context)
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))

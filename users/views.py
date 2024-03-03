2267890efd127d3b2e5647b31540884f32f1bbcf
taluw <electsteve6@gmail.com>
2024-03-03 21:17:33 +0500

    Bound Thug Bound Thug Bound Thug Bound Thug Bound Thug Bound Thug Bound Thug Bound Thug Bound Thug Bound Thug Bound Thug // session carts for anon users

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from carts.models import Cart
 
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
@@ -14,9 +15,15 @@ def login(request):
             username = request.POST['username']
             password = request.POST['password']
             user = auth.authenticate(username=username, password=password)
+
+            session_key = request.session.session_key
+
             if user:
                 auth.login(request, user)
                 messages.success(request, f"Успешный вход в аккаунт {username}")
+
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
 
                 redirect_page = request.POST.get('next', None)
                 if redirect_page and redirect_page != reverse('user:logout'):
@@ -34,13 +41,19 @@ def login(request):
     return render(request, 'users/login.html', context)
 
 def registration(request):
-
     if request.method == 'POST':
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+
+            session_key = request.session.session_key
+
             user = form.instance
             auth.login(request, user)
+
+            if session_key:
+                Cart.objects.filter(session_key=session_key).update(user=user)
+
             messages.success(request, f"Успешная регистрация {user.username}")
             return HttpResponseRedirect(reverse('main:index'))
     else:

a9acafa28e037da488a1fe44c0805a48924d9577
taluw <electsteve6@gmail.com>
2024-03-05 15:54:58 +0500

    действия его шимпанзе подобны, но слова выпускают поток чистого, как хрусталь, разума // admin panel setup

--- goods/admin.py
+++ goods/admin.py
@@ -8,7 +8,21 @@ from goods.models import Categories, Products
 @admin.register(Categories)
 class CategoriesAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
+    list_display = ['name']
+
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
+    list_display = ['name', 'quantity', 'price', 'discount']
+    list_editable = ['quantity', 'price', 'discount']
+    search_fields = ['name', 'description']
+    list_filter = ['discount', 'quantity', 'category']
+    fields = [
+        ("name", "slug"),
+        "category",
+        "description",
+        "image",
+        ("price", "discount"),
+        "quantity",
+    ]

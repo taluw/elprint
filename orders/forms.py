e93c93b789b39129282e91439cb8325d48976fa8
taluw <electsteve6@gmail.com>
2024-03-09 21:36:12 +0500

    Моя бывшая — пройденная миссия Моя будущая пока свободна — это амнистия // custom validator for phone numbers

--- orders/forms.py
+++ orders/forms.py
@@ -1,3 +1,4 @@
+import re
 from django import forms
 
 
@@ -17,6 +18,18 @@ class CreateOrderForm(forms.Form):
         ("1", 'True',),
         ],
     )
+
+    def clean_phone_number(self):
+        data = self.cleaned_data['phone_number']
+
+        if not data.isdigit():
+            raise forms.ValidationError("Номер телефона должен содержать только цифры")
+        #преподготовка шаблона
+        pattern = re.compile(r'^\d{10}$')
+        if not pattern.match(data):
+            raise forms.ValidationError("Номер телефона введён в неверном формате")
+        
+        return data
 
 
 
